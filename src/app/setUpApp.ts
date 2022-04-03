fd6b6ec9fd07985721a11a073edeec664966435b
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-03 19:34:03 +0200

    Added cors configuration

--- src/app/setUpApp.ts
+++ src/app/setUpApp.ts
@@ -2,12 +2,16 @@ import express, { Application, Request, Response } from "express";
 import userRouter from "#user/surfaces/express/routes";
 import authRouter from "src/core/surfaces/express/auth/routes";
 import parseToken from "src/core/surfaces/express/middleware/parseToken";
+import cors from "cors";
 
 const PREFIX = "/api/v1";
 
 const setUpApp = (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
+    app.use(cors({ origin: ["http://localhost:3000"] }));
+
     app.use(parseToken);
 
     //Routes
