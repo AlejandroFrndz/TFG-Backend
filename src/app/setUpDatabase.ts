71dc59493b3e62a79ba12f9ca2ae9b96110d41ef
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-05-02 17:18:39 +0200

    Remove unsued import

--- src/app/setUpDatabase.ts
+++ src/app/setUpDatabase.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import dataSource from "src/core/infra/typeORM/dataSource";
-import { config } from "src/app/config";
 
 const setUpDatabase = async () => {
     await dataSource.initialize();
