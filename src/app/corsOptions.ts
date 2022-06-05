9345729de90d5c4161d47244fc14d2aeaa127bf0
Alex Fernandez <fernandezalcaidealejandro@gmail.com>
2022-06-05 19:03:52 +0200

    Update cors again

--- src/app/corsOptions.ts
+++ src/app/corsOptions.ts
@@ -2,6 +2,6 @@ export const corsOptions = {
     origin: [
         /^http:\/\/localhost:/,
         /^http:\/\/0.0.0.0:/,
-        /^https:\/\/singular-donut-*\.netlify\.app$/
+        "https://singular-donut-014e38.netlify.app"
     ]
 };
