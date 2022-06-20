cab2eb6ba800b1da8ddfa39dafeff4c527878f5c
Alex Fernandez <fernandezalcaidealejandro@gmail.com>
2022-06-20 12:43:34 +0200

    Add new domain to cors options

--- src/app/corsOptions.ts
+++ src/app/corsOptions.ts
@@ -2,6 +2,7 @@ export const corsOptions = {
     origin: [
         /^http:\/\/localhost:/,
         /^http:\/\/0.0.0.0:/,
+        /^https:\/\/.*\.marcotao\.eu$/,
         "https://singular-donut-014e38.netlify.app"
     ]
 };
